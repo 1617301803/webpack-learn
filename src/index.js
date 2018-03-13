b689a483d52a37d81af91a5ca709e71644601446
xuzhao <1617301803@qq.com>
2018-03-13 23:16:43 +0800

    通

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 //import print from './print.js';
 //import common from './common.js';
 
-function getComponet() {
-    return import ( /*webpackChunkName:'lodash'*/ 'lodash').then(_ => {
-        var element = document.createElement('div');
-        element.innerHTML = 'import';
-        return element;
-    }).catch(error => 'error');
+async function getComponet() {
+
+    var element = document.createElement('div');
+    const _ = await
+    import ( /*webpackChunkName:"lodash"*/ 'lodash');
+    element.innerHTML = 'import';
+    return element;
 }
 
 getComponet().then(component => {
