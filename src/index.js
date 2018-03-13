c8b4266668be5bd02659e4bf464cd1eb6ed8ecb5
xuzhao <1617301803@qq.com>
2018-03-13 23:07:32 +0800

    增加wenpackChunkName

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 //import common from './common.js';
 
 function getComponet() {
-    return import ('lodash').then(_ => {
+    return import ( /*webpackChunkName:'lodash'*/ 'lodash').then(_ => {
         var element = document.createElement('div');
         element.innerHTML = 'import';
         return element;
