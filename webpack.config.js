f1f4169e77481c95033dd5c795af722674bd9fd6
xuzhao <1617301803@qq.com>
2018-04-27 21:56:45 +0800

    创建一个webpack的起步

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
+    devtool: 'source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '起步'
+        })
+    ],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
